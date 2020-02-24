0d0a21017916bcd8d244378624180e2ad0c2296b
Francois Chery <swa_isthecreator@Francoiss-MBP.home>
2020-02-24 11:25:00 -0500

    corrected design specs

--- client/src/components/PlayerInfo.js
+++ client/src/components/PlayerInfo.js
@@ -6,15 +6,13 @@ export default function PlayerInfo(props) {
     
     
     return (
-        <div className='playerInfo-container'>
-            <Card className='card'>
-                <div className='info-div'>
-                    <CardTitle className='title'><span className='label'>Name:</span> {props.woman.name}</CardTitle>
-                    <CardText className='p'><span className='label'>Country:</span> {props.woman.country}</CardText>
-                    <CardText className='p'><span className='label'>Searches:</span> {props.woman.searches}</CardText>
-                </div>
-            </Card>
-        </div>
+        <Card className='card'>
+            <div className='info-div'>
+                <CardTitle className='title'><span className='label'>Name:</span> {props.woman.name}</CardTitle>
+                <CardText className='p'><span className='label'>Country:</span> {props.woman.country}</CardText>
+                <CardText className='p'><span className='label'>Searches:</span> {props.woman.searches}</CardText>
+            </div>
+        </Card>
     )
 }
 
--- client/src/tests/PlayerCard.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PlayerCard from '../components/PlayerCard';
-import { render, fireEvent } from "@testing-library/react";
-
-// test('playerCard runs without crashing', () => {
-//     render(<PlayerCard/>);
-// })
